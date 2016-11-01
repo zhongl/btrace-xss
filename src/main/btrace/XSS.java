131f1de90013966572802ad8e6226f63fb7cf4c0
zhongl <zhong.lunfu@gmail.com>
2016-11-01 14:23:38 +0800

    Support uri detecting.

--- src/main/btrace/XSS.java
+++ src/main/btrace/XSS.java
@@ -1,3 +1,4 @@
+import com.sun.btrace.*;
 import com.sun.btrace.annotations.*;
 
 import static com.sun.btrace.BTraceUtils.*;
@@ -7,6 +8,8 @@ public class XSS {
 
     static final Class<?> mapClass  = classForName("java.util.Map");
     static final Class<?> iterClass = classForName("java.lang.Iterable");
+
+    @TLS static Object URI;
 
     @OnMethod(
             clazz = "org.apache.velocity.runtime.parser.node.ASTReference",
@@ -20,10 +23,23 @@ public class XSS {
                 println("Render " + get(field(classOf(self), "nullString"), self) + " -> " + value);
 
                 if (matches("<", str(value))) {
-                    println("Got it : " + value);
+                    println("Got it : " + value + ", from call " +  URI);
                 }
             }
         }
+    }
+
+    @OnMethod(
+            clazz = "javax.servlet.http.HttpServlet",
+            method = "service",
+            location = @Location(Kind.ENTRY)
+    )
+    public static void onService(AnyType[] args) {
+        final AnyType req = args[0];
+        final Object ireq = get(field(classOf(req), "request"), req);
+        final Object iireq = get(field(classOf(ireq), "coyoteRequest"), ireq);
+        final Object uri = get(field(classOf(iireq), "uriMB"), iireq);
+        URI = uri;
     }
 
 }
